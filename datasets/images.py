d49f16cd94cc12c58eb389940c6a7d5cb1011b0b
mhubii <m.huber_1994@hotmail.de>
2020-04-14 01:44:50 +0100

    added imports

--- datasets/images.py
+++ datasets/images.py
@@ -1,3 +1,5 @@
+import cv2
+import PIL
 import pandas as pd
 from torch.utils.data.dataset import Dataset
 
--- train.py
+++ train.py
@@ -1,6 +1,5 @@
-import transforms
-import modelzoo
-import datasets
+from datasets.images import ImageLabel
+from modelzoo.classifiers import ConvClassifier
 
 if __name__ == '__main__':
     pass
